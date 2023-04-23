4b2d30d5e252cc0f76432b509cd6af9f1613f53d
LetoII <cooperharris727@gmail.com>
2023-04-23 11:45:55 -0700

    I added some basic UI that has no functionality but is more so something to look at. It has no art and is useless in nature just wanted to get something done. I also added a camera contorler that allows you to control your movement with both the mouse and the keyboard. The scrolling functionality works perfectly however, I attempted to implement a system that would limit the distance that one could scroll and it did not work. If you have any ideas feel free to implement them. The terrain system works perfectly know. However, you will have to add the objects height in manually as I have not figureed out a way to do it automatically. Furthermore more the system for checking and clearing objects that spawned in the worng area is not working as well. My next step is to try to create a hemisphere linking system where if you go to far up you get teleported to the bottom of the map. Haven't decided weather or not to do this or make it look like a board game sitting on a wooden table. Up to you guys.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Transform followTransform;
+    public static CameraController instance;
+
+    public float normalSpeed;
+    public float fastSpeed; 
+    public float moveSpeed;
+    public float moveTime;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
+
+    public int maxZoomDistance = 150;
+    public int minZoomDistance = -60;
+
+    public Vector3 newPosition;
+    public Quaternion newRotation;
+    public Vector3 newZoom;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosiiton;
+    public Vector3 rotateCurrentPosition;
+
+    void Start()
+    {
+        instance = this;
+        newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    void Update()
+    {   
+        //Follow An Object With the Mouse
+        //Add this code to all moving game objects so that the camera can follow it
+        /*
+        public void OnMouse Down()
+        {
+            CameraController.instance.followTransform = transform;
+        }
+        */
+
+        if(followTransform != null)
+        {
+            transform.position = followTransform.position;
+        }else 
+        {
+            HandelMouseInput();
+            HandelMovementInput();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            followTransform = null;
+        }
+    }
+
+    void HandelMouseInput()
+    {
+        //Scroll With The Mouse
+        if (newZoom.y >= minZoomDistance && newZoom.y <= maxZoomDistance)
+        {
+            if(Input.mouseScrollDelta.y != 0)
+            {
+                newZoom += Input.mouseScrollDelta.y * zoomAmount;
+            }
+        }
+
+        //Pan With the Mouse
+        if(Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if(Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosiiton = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosiiton - rotateCurrentPosition;
+
+            rotateStartPosiiton =  rotateCurrentPosition;
+
+            newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f)); 
+        }
+    }
+
+    void HandelMovementInput()
+    {
+        //Going Faster or Slower with Shift
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }else 
+        {
+            moveSpeed = normalSpeed;
+        }
+
+        //Moving the Camera in All Cardinal Directions
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * moveSpeed);
+        }
+        
+        //Rotation the Camera
+        if(Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        /*
+        //Zooming in and Out
+        if(Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if(Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+        */
+
+        //Smoothing Out All Movement and Input
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * moveTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * moveTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * moveTime);
+    }
+}
+
