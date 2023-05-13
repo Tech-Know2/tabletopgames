3fba6207b6980a7bace9f8e0f3a45ff9ab67ecd8
LetoII <cooperharris727@gmail.com>
2023-05-12 20:21:50 -0700

    Added a bunch of temp Ui waiting for Nick to send over desired desing for the Tech tree and overall user interface, just leave the UI disabled and turned off like it is in the project. Also created a series of scriptable objects that will help us maintina control over the projects techs and crads, and will create a modular system for their creation and management. These can be changed, and will be changed as we find out more about the systems that we need to have in place in order for these things to work properly. Enjoy!

---
+++ Assets/Scripts/Cards/CardDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Text cardType;
+
+    public Image artworkImage;
+
+    public Text costText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
