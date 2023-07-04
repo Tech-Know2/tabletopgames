2af2df5f9c2dc5266b78ce96d6da92847fcd42a1
LetoII <cooperharris727@gmail.com>
2023-07-04 09:02:54 -0700

    Revert "Attempted to fix some back end unity changed (Revert to the one before if everything breaks)"
    This reverts commit 039e1b3a10808f9097af796c20d7954f4fcbe446.

--- Assets/Assets/Scripts/Auction House/AuctionHouseController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AuctionHouseController : MonoBehaviour
-{
-    //Display Controller for the Auction House
-}
--- Assets/Assets/Scripts/Tech Scripts/ClonedTechData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ClonedTechData : MonoBehaviour
-{
-    //Data Sourced From Researched Techs Just In Case We Ever End Up Needing It
-    public List<Tech> clonedResearchedTechs = new List<Tech>();
-}
